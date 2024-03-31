964e88789f155a9ff7c768bcc69acc67513d5732
kashif malik <malikashifali3452@gmail.com>
2024-03-31 21:38:29 +0500

    gettting value from input field rather than prompt

--- js/script.js
+++ js/script.js
@@ -5,47 +5,48 @@ let documents = document.getElementById("output")
 
 
 // clear output button 
-function clearOutput (){
+function clearOutput() {
     documents.innerHTML = " "
 }
 
 // simple alert 
-function simpleAlert(){
+function simpleAlert() {
     alert("alert button is clicked")
     documents.innerHTML = "alert button is clicked"
 }
 
 // print my name 
-function printName (){
-    let userName = prompt("please enter your name")
-    documents.innerHTML = userName ;
+function printName() {
+    let userName = document.getElementById("inputField").value;
+    documents.innerHTML = userName;
 }
 
 // print all cities 
-function printAllCities(){
+function printAllCities() {
     documents.innerHTML = " "
-    for (let i=0; i<cities.length; i++){
-        documents.innerHTML += i+1 +") " + cities[i] + "<br />"
+    for (let i = 0; i < cities.length; i++) {
+        documents.innerHTML += i + 1 + ") " + cities[i] + "<br />"
     }
 }
 
 // add city in list 
-function addCity(){
+function addCity() {
     documents.innerHTML = " "
-    let newCity = prompt("please enter city name you want to add")
+    let newCity = document.getElementById("inputField").value;
+    console.log(newCity)
     cities.push(newCity)
 
-    for (let i=0; i<cities.length; i++){
-        documents.innerHTML += i+1 +")" + cities[i] + "<br />"
+    for (let i = 0; i < cities.length; i++) {
+        documents.innerHTML += i + 1 + ") " + cities[i] + "<br />"
     }
 }
 
 // generate table 
-function generateTable(){
-    tableNumber = +prompt("please enter table number that use want to generate")
+function generateTable() {
+    let tableNumber = document.getElementById("inputField").value;
     documents.innerHTML = `<h3>Table of ${tableNumber}<h3/> <hr />`
-    for(let i=1; i<11; i++){
-        let table = tableNumber + " * " + i + " = " + tableNumber*i + "<br />"
-        documents.innerHTML += table ;
+    for (let i = 1; i < 11; i++) {
+        let table = tableNumber + " * " + i + " = " + tableNumber * i + "<br />"
+        documents.innerHTML += table;
     }
 }
